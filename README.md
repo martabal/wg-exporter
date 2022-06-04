8e7f83f69e03ec1b352308b1e132b50581e09bf8
Martin <martin.labat92@gmail.com>
2022-06-04 11:15:49 +0200

    first edit

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 # wg-exporter
-[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/C0C8AHIE0)
+
 
 *wg-exporter* is a simple yet effective Prometheus exporter for Wireguard.
 
 ## What are the collected metrics ?
-- General:
-    - `wg_connected_peers`: number of connected peers to the VPN server
+
 - Server:
     - `wg_server_info{pkey, interface}`: the listen port of the VPN server, labeled by the public key of the server, and the interface name
 - Peers: (All labeled by the public key of the peer and the interface name connected to)
     - `wg_peer_latest_handshake{pkey, interface}`: unit timestamp of the last handshake
     - `wg_peer_transfer_rx{pkey, interface}`: data received in bytes
     - `wg_peer_transfer_tx{pkey, interface}`: data transmitted in bytes
-    - `wg_peer_connection_status{pkey, interface}`: is the peer connected now ? 0 for False, 1 for True
 
 ## Install
 Clone the project
 ```bash
-git clone git@github.com:men1n2/wg-exporter.git
+git clone git@github.com:martabal/wg-exporter.git
 ```
 
 ## Usage
@@ -35,3 +33,13 @@ scrape_configs:
     static_configs:
       - targets: ["localhost:8400"]
 ```
+
+
+> **NOTE:** Removed ```wg_peer_connection_status``` to stop ping last peer
+
+
+## Grafana 
+
+You can import the dashboard in grafana/dashboard.json to Grafana to monitor easely your wireguard activity !
+
+![The dashboard](/grafana/dashboard.png "Wireguard dashboard")
--- src/app.py
+++ src/app.py
@@ -15,12 +15,10 @@ class wg_metrics_handler(BaseHTTPRequestHandler):
     def parse_wg_output(self, wg_output):
         # Check the doc for the output format: https://manpages.debian.org/unstable/wireguard-tools/wg.8.en.html#show
         self.wg_interface = ''
-        self.connected_peers = 0
         lines = wg_output.splitlines()
         parsed_result = self.parse_server_info(lines[0].split('\t'))
         for peer_info in lines[1:]: # will be empty if no peers
             parsed_result += self.parse_peer_info(peer_info.split('\t'))
-        parsed_result += f'wg_connected_peers{{interface="{self.wg_interface}"}} {self.connected_peers}\n'
         return parsed_result
     
     def parse_server_info(self, server_info):
@@ -31,17 +29,12 @@ class wg_metrics_handler(BaseHTTPRequestHandler):
         peer_latest_handshake = f'wg_peer_latest_handshake{{pkey="{peer_info[1]}", interface="{peer_info[0]}"}} {peer_info[5]}\n'
         peer_latest_transfer_rx = f'wg_peer_transfer_rx{{pkey="{peer_info[1]}", interface="{peer_info[0]}"}} {peer_info[6]}\n'
         peer_latest_transfer_tx = f'wg_peer_transfer_tx{{pkey="{peer_info[1]}", interface="{peer_info[0]}"}} {peer_info[7]}\n'
-        peer_ip_address = peer_info[4].split('/')[0]
-        is_peer_up = 1 if subprocess.run(['ping', '-W', '1', '-c', '1', '-t','1', peer_ip_address], capture_output=True).returncode == 0 else 0
-        peer_connection_status = f'wg_peer_connection_status{{pkey="{peer_info[1]}", interface="{peer_info[0]}"}} {is_peer_up}\n'
-        if(is_peer_up):
-            self.connected_peers += 1
-        return peer_latest_handshake + peer_latest_transfer_rx + peer_latest_transfer_tx + peer_connection_status
+        return peer_latest_handshake + peer_latest_transfer_rx + peer_latest_transfer_tx
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--bind', type=str, help='The interface to bind the server to. Defaults to localhost', default='127.0.0.1')
+    parser.add_argument('--bind', type=str, help='The interface to bind the server to. Defaults to localhost', default='')
     parser.add_argument('--port', type=int, help='The port to listen to', default=8400)
     args = parser.parse_args()
     with HTTPServer((args.bind, args.port), wg_metrics_handler) as server:
